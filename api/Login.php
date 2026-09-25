09422631c7e16a550365f3e43fc5d8b512282152
MelindaM4y <melinda.mai24@gmail.com>
2026-09-25 10:10:49 -0400

    Add function skeleton for login API

---
+++ api/Login.php
@@ -0,0 +1,24 @@
+
+<?php
+
+	function getRequestInfo()
+	{
+		
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		
+	}
+	
+	function returnWithError( $err )
+	{
+		
+	}
+	
+	function returnWithInfo( $firstName, $lastName, $id )
+	{
+		
+	}
+	
+?>
